486556517e737a757d5d6a106a789cd029a401dd
dpcairns <dpcairns@gmail.com>
2017-09-10 15:00:45 -0700

    missing script tag

--- server/render.js
+++ server/render.js
@@ -29,7 +29,7 @@ export default ({ clientStats }) => async (req, res) => {
           <script>window.REDUX_STATE = ${stateJson}</script>
           <div id="root">${html}</div>
           <script type='text/javascript' src='/static/vendor.js'></script>
-          ${Js}
+          <script>${Js}</script>
         </body>
       </html>`,
   );
